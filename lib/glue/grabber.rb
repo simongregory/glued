53e771fa606cf15f1cc1c5b358c0a5c74f17174f
Simon Gregory <simon@helvector.org>
2014-01-28 15:56:48 +0000

    Grabber now lists all fragment urls

--- lib/glue/grabber.rb
+++ lib/glue/grabber.rb
@@ -3,11 +3,25 @@
 class Grabber
   attr_reader :urls
 
-  def initialize(man)
-    @manifest = man
+  def initialize(manifest, bootstrap)
+    raise "Only one segment can be handled" if bootstrap.segments != 1 #As we've hardcoded 1 below
+    raise "Not enough fragments" if bootstrap.fragments < 1
+    raise "Too many fragments" if bootstrap.fragments > 2000 #not sure what this limit should be
+
+    @url = "#{manifest.base_ref}/#{manifest.media_filename}Seg1-Frag"
+    @total_fragments = bootstrap.fragments
     @urls = []
 
-    @urls << 'foo'
+    build
+  end
+
+  private
+  def build
+    fragment = 1
+    while fragment <= @total_fragments
+      @urls << "#{@url}#{fragment}"
+      fragment += 1
+    end
   end
 
 end
--- spec/grabber_spec.rb
+++ spec/grabber_spec.rb
@@ -4,11 +4,36 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "grabbing fragments" do
 
-  it "builds a nice long list of url fragments" do
-    f4m = double(F4M)
-    grabber = Grabber.new(f4m)
+  before(:each) do
+    @bs = double(Bootstrap)
+    @f4m = double(F4M)
+  end
 
-    grabber.urls.should_not be_empty
+  after(:each) do
+    @bs = nil
+    @f4m = nil
+  end
+
+  it "builds a nice long list of url fragments" do
+    @f4m.stub(:base_ref).and_return('http://the.young.ones')
+    @f4m.stub(:media_filename).and_return('some-audio-video-')
+    @bs.stub(:segments).and_return(1)
+    @bs.stub(:fragments).and_return(20)
+
+    grabber = Grabber.new(@f4m, @bs)
+
+    expect(grabber.urls.first).to eq('http://the.young.ones/some-audio-video-Seg1-Frag1')
+    expect(grabber.urls.last).to eq('http://the.young.ones/some-audio-video-Seg1-Frag20')
+  end
+
+  it "fails without the correct number of segments" do
+    @bs.stub(:segments).and_return(0)
+    expect { Grabber.new(@f4m, @bs) }.to raise_error "Only one segment can be handled"
+
+    @bs.stub(:segments).and_return(1)
+    @bs.stub(:fragments).and_return(0)
+
+    expect { Grabber.new(@f4m, @bs) }.to raise_error "Not enough fragments"
   end
 
   it "downloads the nice long list of fragments" do
